7331ca2ccc74b3cb9b879aa1545581f924529984
ritafomkina <ritakisil@mail.ru>
2023-07-23 18:50:33 +0300

    add vercel statistics

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { inject } from '@vercel/analytics';
 import AppModule from './app/app.module';
+
+inject();
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   // eslint-disable-next-line no-console
