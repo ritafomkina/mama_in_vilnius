58b27ae00118c1cb7addc729f2ad3f3b34689616
ritafomkina <fomkina.margarita@m.thelightech.com>
2025-05-11 08:58:18 +0300

    refactor: routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,27 @@ import { Component, OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { APP_LOCATION_TOKEN } from './core/tokens/app-location-token';
 import { Location } from '@angular/common';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { LOADING } from './core/tokens/loading.token';
+import { FooterComponent } from './ui/footer/footer.component';
+import { HeaderComponent } from './ui/header/header.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    template: ` <router-outlet></router-outlet> `,
-    imports: [RouterOutlet],
+    templateUrl: './app.template.html',
+    styleUrl: './app.style.scss',
+    imports: [
+        RouterOutlet,
+        MatProgressSpinnerModule,
+        HeaderComponent,
+        FooterComponent,
+    ],
 })
 export class AppComponent implements OnInit {
     private readonly _location = inject(Location);
     private readonly _appLocation = inject(APP_LOCATION_TOKEN);
+    readonly isLoading = inject(LOADING).state;
 
     ngOnInit(): void {
         const currentPath = this._location.path();
