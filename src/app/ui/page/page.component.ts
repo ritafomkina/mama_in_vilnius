a69ceaa07d50a228ebfe53bc92a4d764a5582508
Vladimir Grigorishin <volodka.grigor@gmail.com>
2025-06-11 23:40:14 +0300

    Display article in the page component

--- src/app/ui/page/page.component.ts
+++ src/app/ui/page/page.component.ts
@@ -1,26 +1,71 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { ActivatedRoute, RouterLink, RouterOutlet } from '@angular/router';
+import {
+    ChangeDetectionStrategy,
+    Component,
+    OnDestroy,
+    signal,
+} from '@angular/core';
+import {
+    ActivatedRoute,
+    RouterLink,
+    RouterLinkActive,
+    RouterOutlet,
+} from '@angular/router';
 
-import { toSignal } from '@angular/core/rxjs-interop';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 
-import { map } from 'rxjs';
+import { map, of, Subject, switchMap, takeUntil } from 'rxjs';
 
-import { Topic } from '@models';
+import { Article, Topic } from '@models';
+import { ArticleService } from '@core/services';
+import { ArticleComponent } from '@ui';
 
 @Component({
     selector: 'app-page',
     standalone: true,
-    imports: [RouterOutlet, RouterLink, MatButtonModule, MatIconModule],
+    imports: [
+        RouterOutlet,
+        RouterLink,
+        MatButtonModule,
+        MatIconModule,
+        ArticleComponent,
+        RouterLinkActive,
+    ],
     templateUrl: './page.component.html',
     styleUrl: './page.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PageComponent {
-    private activatedRoute = inject(ActivatedRoute);
+export class PageComponent implements OnDestroy {
+    public topic = signal<Topic | undefined>(undefined);
+    public article = signal<Article | undefined>(undefined);
 
-    public topic = toSignal(
-        this.activatedRoute.data.pipe(map(({ topic }) => topic as Topic)),
-    );
+    private unsubscribe$ = new Subject<void>();
+
+    constructor(
+        activatedRoute: ActivatedRoute,
+        articleService: ArticleService,
+    ) {
+        activatedRoute.data
+            .pipe(
+                takeUntil(this.unsubscribe$),
+                map(({ topic }) => {
+                    this.topic.set(topic);
+                    return topic as Topic;
+                }),
+                switchMap(({ topicPath }) =>
+                    topicPath
+                        ? articleService.getArticle(topicPath)
+                        : of(undefined),
+                ),
+                map((article) => {
+                    this.article.set(article);
+                }),
+            )
+            .subscribe();
+    }
+
+    public ngOnDestroy(): void {
+        this.unsubscribe$.next();
+        this.unsubscribe$.complete();
+    }
 }
