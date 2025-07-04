04143eb04f01d7238406fbe5f779cef3e75441a5
ritafomkina <margarita_fomkina@epam.com>
2025-07-04 21:53:44 +0300

    feat: about-me

--- src/app/start-info/start-info.component.ts
+++ src/app/start-info/start-info.component.ts
@@ -1,8 +1,14 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+    AfterViewInit,
+    ChangeDetectionStrategy,
+    Component,
+    inject,
+} from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-
+import { MatDialog } from '@angular/material/dialog';
+import { StartDialogComponent } from '@ui';
 @Component({
     selector: 'app-start-info',
     standalone: true,
@@ -11,4 +17,16 @@ import { MatButtonModule } from '@angular/material/button';
     imports: [RouterLink, MatButtonModule, MatIconModule],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class StartInfoComponent {}
+export default class StartInfoComponent implements AfterViewInit {
+    readonly dialog = inject(MatDialog);
+
+    ngAfterViewInit(): void {
+        if (localStorage.getItem('startDialogClosed') === 'true') {
+            return;
+        }
+        this.dialog.open(StartDialogComponent, {
+            width: '500px',
+            autoFocus: false,
+        });
+    }
+}
--- src/app/ui/footer/footer.component.ts
+++ src/app/ui/footer/footer.component.ts
@@ -1,10 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-footer',
     standalone: true,
     templateUrl: 'footer.template.html',
     styleUrl: 'footer.style.scss',
+    imports: [RouterLink],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FooterComponent {}
