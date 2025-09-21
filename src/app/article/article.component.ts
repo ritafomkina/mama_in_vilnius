d3ac5da009ee4de06defcfe589e065a5060d4721
Vladimir Grigorishin <volodka.grigor@gmail.com>
2025-09-21 13:09:37 +0300

    Add markdown rendering and rewrite help-ukrainian-moms.json to md

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,4 +1,6 @@
 import { Component, inject, input } from '@angular/core';
+import { NgOptimizedImage, Location } from '@angular/common';
+
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import {
@@ -8,12 +10,14 @@ import {
     MatSnackBarRef,
     SimpleSnackBar,
 } from '@angular/material/snack-bar';
-import { NgOptimizedImage } from '@angular/common';
-import { Article } from '../models/article.interface';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
-import { Location } from '@angular/common';
+
 import { Clipboard } from '@angular/cdk/clipboard';
+
+import markdownit from 'markdown-it';
+
+import { Article } from '../models/article.interface';
 
 @Component({
     selector: 'app-article',
@@ -30,6 +34,44 @@ import { Clipboard } from '@angular/cdk/clipboard';
     styleUrl: 'article.style.scss',
 })
 export class ArticleComponent {
+    // full options list (defaults)
+    public md = markdownit({
+        // Enable HTML tags in source
+        html: false,
+
+        // Use '/' to close single tags (<br />).
+        // This is only for full CommonMark compatibility.
+        xhtmlOut: false,
+
+        // Convert '\n' in paragraphs into <br>
+        breaks: false,
+
+        // CSS language prefix for fenced blocks. Can be
+        // useful for external highlighters.
+        langPrefix: 'language-',
+
+        // Autoconvert URL-like text to links
+        linkify: false,
+
+        // Enable some language-neutral replacement + quotes beautification
+        // For the full list of replacements, see https://github.com/markdown-it/markdown-it/blob/master/lib/rules_core/replacements.mjs
+        typographer: false,
+
+        // Double + single quotes replacement pairs, when typographer enabled,
+        // and smartquotes on. Could be either a String or an Array.
+        //
+        // For example, you can use '«»„“' for Russian, '„“‚‘' for German,
+        // and ['«\xA0', '\xA0»', '‹\xA0', '\xA0›'] for French (including nbsp).
+        quotes: '“”‘’',
+
+        // Highlighter function. Should return escaped HTML,
+        // or '' if the source string is not changed and should be escaped externally.
+        // If result starts with <pre... internal wrapper is skipped.
+        highlight: function (/*str, lang*/) {
+            return '';
+        },
+    });
+
     private readonly _location: Location = inject(Location);
     private readonly _clipboard: Clipboard = inject(Clipboard);
     private readonly _snackbar: MatSnackBar = inject(MatSnackBar);
@@ -41,6 +83,26 @@ export class ArticleComponent {
         this._config.duration = 2000;
         this._config.horizontalPosition = 'end';
         this._config.verticalPosition = 'top';
+
+        const defaultRender =
+            this.md.renderer.rules['link_open'] ||
+            function (tokens, idx, options, env, self) {
+                return self.renderToken(tokens, idx, options);
+            };
+
+        this.md.renderer.rules['link_open'] = function (
+            tokens,
+            idx,
+            options,
+            env,
+            self,
+        ) {
+            // Add a new `target` attribute, or replace the value of the existing one.
+            tokens[idx].attrSet('target', '_blank');
+
+            // Pass the token to the default renderer.
+            return defaultRender(tokens, idx, options, env, self);
+        };
     }
 
     copyLink() {
--- src/app/models/article.interface.ts
+++ src/app/models/article.interface.ts
@@ -1,4 +1,5 @@
 export interface Article {
     title: string;
-    html: string;
+    html?: string;
+    md?: string;
 }
